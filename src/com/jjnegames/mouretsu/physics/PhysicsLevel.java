bde67747fdd642e90476b57e324665b73df4eb7f
Joni Halonen <jonpethal@gmail.com>
2014-10-24 19:15:39 +0300

    Fail push

--- src/com/jjnegames/mouretsu/physics/PhysicsLevel.java
+++ src/com/jjnegames/mouretsu/physics/PhysicsLevel.java
@@ -1,5 +1,9 @@
 package com.jjnegames.mouretsu.physics;
 
-public class PhysicsLevel {
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.World;
 
+public class PhysicsLevel {
+	Vec2 gravity = new Vec2(0,1);
+	World world = new World(null);
 }
