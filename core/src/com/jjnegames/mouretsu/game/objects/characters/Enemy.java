5c1ab59c7066463ec54c00699e7543b71a0a1482
s1200572 <s1200572@WS1200572.bc.local>
2015-02-11 10:32:16 +0200

    Enemy movement

--- core/src/com/jjnegames/mouretsu/game/objects/characters/Enemy.java
+++ core/src/com/jjnegames/mouretsu/game/objects/characters/Enemy.java
@@ -1,15 +1,23 @@
 package com.jjnegames.mouretsu.game.objects.characters;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
+import com.jjnegames.mouretsu.game.MGame;
 import com.jjnegames.mouretsu.game.objects.GameObject;
 import com.jjnegames.mouretsu.game.objects.Rect;
 
 
 	public class Enemy extends Char	{
+		
+		float counter = 0;
+		boolean oikeelle = false;
+		
 	public Enemy(Body body, Texture texture, float width, float height) {
 		super(body,texture);
 		
@@ -17,6 +25,8 @@ import com.jjnegames.mouretsu.game.objects.Rect;
 		this.setOriginY(height/2);
 		this.setWidth(width);
 		this.setHeight(height);
+		
+		
 	}
 	
 	public static Enemy create(World world, BodyDef bodyDef3, float w, float h,
@@ -39,6 +49,19 @@ import com.jjnegames.mouretsu.game.objects.Rect;
 	@Override
 	protected void childUpdate(float delta) {
 		
+	counter += delta;
+		
+		 if (counter>3){
+			// oikeelle = !oikeelle;
+			body.setLinearVelocity(new Vector2(-4,body.getLinearVelocity().y));
+		} if (counter>6) {
+			
+			body.setLinearVelocity(new Vector2(4,body.getLinearVelocity().y));
+		} if (counter>8) {
+			counter = 0;
+		}
+		
+		World world = MGame.world;
 		
 	}
 
